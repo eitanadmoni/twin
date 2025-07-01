690ec5c4e2428343bc79fbd8adcfe4124f2014fb
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-07-01 17:48:51 +0300

    fix technai

--- ttwin/technai.cpp
+++ ttwin/technai.cpp
@@ -2,29 +2,55 @@
 #include <mutex>
 #include <thread>
 #include <chrono>
+#include <iostream>
 
-
+using std::cout;
 std::mutex lock;
-static bool first = TRUE;
 
-void setRegistry() {
+
+/*
+* Function to set the executable of the message box to the run registry so it will execute will the computer is booted
+*/
+void setRunRegistry() {
 	HKEY hkey;
-	PHKEY phkResult = &hkey;
-	RegOpenKeyA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", phkResult);
-	RegSetValueExA(hkey, "messageBox", 0, REG_SZ, (const BYTE*)"C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp", sizeof("C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp") + 1);
+	LSTATUS open_status = RegOpenKeyA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", &hkey);
+	if (open_status != ERROR_SUCCESS) {
+		cout << "opening failed\n";
+	}
+	
+	LSTATUS writing_status = RegSetValueExA(hkey, "messageBox", 0, REG_SZ, (const BYTE*)"C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp", sizeof("C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp") + 1);
+	if (writing_status != ERROR_SUCCESS) {
+		cout << "writing failed\n";
+	}
 }
 
+
 int main() {
-	if (first) {
-		setRegistry();
-		first = FALSE;
+	HANDLE mutex_handler;
+	LPCSTR message = "MANAGEMENT PROGRAM IS UP";
+	setRunRegistry(); //make sure that the message-box executable is in the run registry
+	
+	mutex_handler = CreateMutexA(NULL, false, "tech_mutex");
+	if (mutex_handler == NULL) {
+		cout << "Creating mutex failed\n";
+		return 1;
 	}
-	if (lock.try_lock()) {
-		LPCSTR message = "MANAGEMENT PROGRAM IS UP";
-		MessageBoxA(NULL, message, "message", MB_OK);
-		std::this_thread::sleep_for(std::chrono::hours(1));
-		lock.unlock();
-		return 0;
+	DWORD dwWaitResult = WaitForSingleObject(
+		mutex_handler,    
+		INFINITE);  
+
+	switch (dwWaitResult) {
+		case WAIT_OBJECT_0:
+			MessageBoxA(NULL, message, "message", MB_OK);
+			Sleep((DWORD)3600000);
+			if (!ReleaseMutex(mutex_handler))
+			{
+				cout << "Failed to release mutex\n";
+				return 1;
+			}
+		case WAIT_ABANDONED:
+			cout << "The mutex is get abandoned!\n";
+			return 1;
 	}
-	return 1;
+	return 0;
 }
