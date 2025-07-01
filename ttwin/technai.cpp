c8c9ac46504a3ac229ccd3ddbf2b128c5602e6dd
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-07-01 19:33:48 +0300

    fix technai

--- ttwin/technai.cpp
+++ ttwin/technai.cpp
@@ -3,49 +3,76 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <string>
 
 using std::cout;
-std::mutex lock;
+using std::string;
 
+enum error_code {
+	OPENING_ERROR,
+	WRITING_ERROR,
+};
+
+#define MESSAGE_BOX_PATH "C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp"
 
 /*
-* Function to set the executable of the message box to the run registry so it will execute will the computer is booted
+* @brief Set the executable of the message box to the run registry so it will execute when the computer is booted
 */
 void setRunRegistry() {
+
 	HKEY hkey;
-	LSTATUS open_status = RegOpenKeyA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", &hkey);
-	if (open_status != ERROR_SUCCESS) {
-		cout << "opening failed\n";
+	LSTATUS openStatus = RegOpenKeyA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", &hkey);
+	if (openStatus != ERROR_SUCCESS) {
+		throw OPENING_ERROR;
 	}
 	
-	LSTATUS writing_status = RegSetValueExA(hkey, "messageBox", 0, REG_SZ, (const BYTE*)"C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp", sizeof("C:\\Users\\user\\source\\repos\\eitanadmoni\\twin\\ttwin\\technai.cpp") + 1);
-	if (writing_status != ERROR_SUCCESS) {
-		cout << "writing failed\n";
+	LSTATUS writingStatus = RegSetValueExA(hkey, "messageBox", 0, REG_SZ, (const BYTE*)MESSAGE_BOX_PATH, sizeof(MESSAGE_BOX_PATH) + 1);
+	if (writingStatus != ERROR_SUCCESS) {
+		throw WRITING_ERROR;
 	}
 }
 
 
 int main() {
-	HANDLE mutex_handler;
+	HANDLE mutexHandler;
 	LPCSTR message = "MANAGEMENT PROGRAM IS UP";
-	setRunRegistry(); //make sure that the message-box executable is in the run registry
-	
-	mutex_handler = CreateMutexA(NULL, false, "tech_mutex");
-	if (mutex_handler == NULL) {
+	try {
+		setRunRegistry(); //make sure that the message-box executable is in the run registry
+	}
+	catch (error_code errorCode) {
+		if (errorCode == OPENING_ERROR) {
+			cout << "Fail to open the registery\n";
+			return 1;
+		}
+		else if (errorCode == WRITING_ERROR) {
+			cout << "Fail to write to the registry\n";
+			return 1;
+		}
+		else {
+			cout << "Unknown error\n";
+			return 1;
+		}
+	}
+	mutexHandler = CreateMutexA(NULL, FALSE, "tech_mutex");
+	if (mutexHandler == NULL) {
 		cout << "Creating mutex failed\n";
 		return 1;
 	}
 	DWORD dwWaitResult = WaitForSingleObject(
-		mutex_handler,    
+		mutexHandler,    
 		INFINITE);  
 
 	switch (dwWaitResult) {
 		case WAIT_OBJECT_0:
 			MessageBoxA(NULL, message, "message", MB_OK);
 			Sleep((DWORD)3600000);
-			if (!ReleaseMutex(mutex_handler))
+			if (!ReleaseMutex(mutexHandler))
 			{
 				cout << "Failed to release mutex\n";
+				return 1;
+			}
+			if (!CloseHandle(mutexHandler)) {
+				cout << "Failed to close mutex handler\n";
 				return 1;
 			}
 		case WAIT_ABANDONED:
