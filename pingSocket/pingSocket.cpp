c9ba4988bd1d59d0f7a6290069f51456480b2817
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-07-02 17:45:23 +0300

    ping socket

--- pingSocket/pingSocket.cpp
+++ pingSocket/pingSocket.cpp
@@ -4,6 +4,7 @@
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS 
 
+#include "Exception.h"
 
 #include <windows.h>
 #include <winsock2.h>
@@ -17,21 +18,37 @@
 // #pragma comment (lib, "Mswsock.lib")
 
 
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015"
+enum returnValue {
+    SUCCESS,
+    FAILURE,
+};
+
+
+const int DEFAULT_BUFLEN = 512;
 
 
 using std::string;
+using std::cerr;
+using std::endl;
 
-
+/*
+* @brief finction to clean buffer and fill it with null terminator
+* @param buffer a array to fill with null terminator
+* @param size the size of the array
+* @param size the size of the array
+*/
 void cleanBuf(char* buffer, int size) {
     for (int i = 0; i < size; i++) {
         buffer[i] = '\0';
     }
 }
 
-int __cdecl main(void)
-{
+
+/*
+* @brief open a server that listen for ping and return pong
+* @return value that tell if the program succeeded
+*/
+int pingServerSocket(PCSTR port) {
     WSADATA wsaData;
     int iResult;
     int* lenPointer;
@@ -43,120 +60,130 @@ int __cdecl main(void)
     struct addrinfo hints;
 
     int iSendResult;
-    char recvBuf[DEFAULT_BUFLEN] = { '\0' } ;
+    char recvBuf[DEFAULT_BUFLEN] = { '\0' };
     const string pong = "PONG";
     const string ping = "PING";
     int recvBuflen = DEFAULT_BUFLEN;
 
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed with error: %d\n", iResult);
-        return 1;
-    }
-
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE;
-
-    // Resolve the server address and port
-    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-    if (iResult != 0) {
-        printf("getaddrinfo failed with error: %d\n", iResult);
-        WSACleanup();
-        return 1;
-    }
-
-    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (ListenSocket == INVALID_SOCKET) {
-        printf("socket failed with error: %ld\n", WSAGetLastError());
-        freeaddrinfo(result);
-        WSACleanup();
-        return 1;
-    }
-
-    // Setup the TCP listening socket
-    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (iResult == SOCKET_ERROR) {
-        printf("bind failed with error: %d\n", WSAGetLastError());
-        freeaddrinfo(result);
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    freeaddrinfo(result);
-
-    iResult = listen(ListenSocket, SOMAXCONN);
-    if (iResult == SOCKET_ERROR) {
-        printf("listen failed with error: %d\n", WSAGetLastError());
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-    
-    while (true) {
-        // Accept a client socket
-        ClientSocket = accept(ListenSocket, NULL, NULL);
-        if (ClientSocket == INVALID_SOCKET) {
-            printf("accept failed with error: %d\n", WSAGetLastError());
-            closesocket(ListenSocket);
-            break;
+    try {
+        iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (iResult != 0) {
+            throw Exception("WSAStartup failed");
         }
 
-        // No longer need server socket
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        hints.ai_flags = AI_PASSIVE;
 
-        // Receive until the peer shuts down the connection
-        do {
-            cleanBuf(recvBuf, std::size(recvBuf));
-            iResult = recv(ClientSocket, recvBuf, 4, 0);
-            if (iResult > 0) {
-                lenPointer = (int*)recvBuf;
-                messageLen = lenPointer[0];
-                Sleep(1);
+        // Resolve the server address and port
+        iResult = getaddrinfo(NULL, port, &hints, &result);
+        if (iResult != 0) {
+            WSACleanup();
+            throw Exception("getaddrinfo failed");
+        }
+
+        ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+        if (ListenSocket == INVALID_SOCKET) {
+            freeaddrinfo(result);
+            WSACleanup();
+            throw Exception("Listen socket failed");
+        }
+
+        // Setup the TCP listening socket
+        iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            freeaddrinfo(result);
+            closesocket(ListenSocket);
+            WSACleanup();
+            throw Exception("bind failed");
+        }
+
+        freeaddrinfo(result);
+
+        iResult = listen(ListenSocket, SOMAXCONN);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ListenSocket);
+            WSACleanup();
+            throw Exception("listen failed");
+        }
+
+        while (true) {
+            // Accept a client socket
+            ClientSocket = accept(ListenSocket, NULL, NULL);
+            if (ClientSocket == INVALID_SOCKET) {
+                closesocket(ListenSocket);
+                throw Exception("accept failed");
+            }
+
+
+            // Receive until the peer shuts down the connection
+            do {
                 cleanBuf(recvBuf, std::size(recvBuf));
-                iResult = recv(ClientSocket, recvBuf, messageLen, 0);
-                if (ping == (string)recvBuf) {
-                    printf("PING received\n");
-                    iSendResult = send(ClientSocket, pong.c_str(), iResult, 0);
-                    if (iSendResult == SOCKET_ERROR) {
-                        printf("send failed with error: %d\n", WSAGetLastError());
+                iResult = recv(ClientSocket, recvBuf, 4, 0);
+                if (iResult > 0) {
+                    lenPointer = (int*)recvBuf;
+                    messageLen = lenPointer[0];
+                    cleanBuf(recvBuf, std::size(recvBuf));
+                    iResult = recv(ClientSocket, recvBuf, messageLen, 0);
+                    if (ping == (string)recvBuf) {
+                        printf("PING received\n");
+                        iSendResult = send(ClientSocket, pong.c_str(), iResult, 0);
+                        if (iSendResult == SOCKET_ERROR) {
+                            printf("send failed with error: %d\n", WSAGetLastError());
+                        }
                     }
+                    else {
+                        printf("Unknown message\n");
+                        iSendResult = send(ClientSocket, "Unknown message", std::size("Unknown message"), 0);
+                        if (iSendResult == SOCKET_ERROR) {
+                            printf("send failed with error: %d\n", WSAGetLastError());
+                        }
+                    }
+
+                }
+
+                else if (iResult == 0) {
+                    printf("Connection closing...\n");
                 }
                 else {
-                    printf("Unknown message\n");
-                    iSendResult = send(ClientSocket, "Unknown message", std::size("Unknown message"), 0);
-                    if (iSendResult == SOCKET_ERROR) {
-                        printf("send failed with error: %d\n", WSAGetLastError());
-                    }
+                    printf("recv failed with error: %d\n", WSAGetLastError());
+                    break;
                 }
+            } while (iResult > 0);
 
+            // shutdown the connection since we're done
+            iResult = shutdown(ClientSocket, SD_SEND);
+            if (iResult == SOCKET_ERROR) {
+                closesocket(ClientSocket);
+                throw Exception("shutdown failed");
             }
-            
-            else if (iResult == 0) {
-                printf("Connection closing...\n");
-            }
-            else {
-                printf("recv failed with error: %d\n", WSAGetLastError());
-                break;
-            }
-        } while (iResult > 0);
 
-        // shutdown the connection since we're done
-        iResult = shutdown(ClientSocket, SD_SEND);
-        if (iResult == SOCKET_ERROR) {
-            printf("shutdown failed with error: %d\n", WSAGetLastError());
+            // cleanup
             closesocket(ClientSocket);
-            break;
         }
-
-        // cleanup
-        closesocket(ClientSocket);
     }
 
+    catch (const Exception& e) {
+        closesocket(ListenSocket);
+        WSACleanup();
+        cerr << e.getError() << endl;
+        return FAILURE;
+    }
     closesocket(ListenSocket);
     WSACleanup();
-    return 0;
+    return SUCCESS;
+
+    return SUCCESS;
+}
+
+
+int __cdecl main(int argc, char** argv){
+    if (argc != 2) {
+        cerr << "Number of arguments need to be exactly 1 - port to listen at" << endl;
+        return FAILURE;
+    }
+    PCSTR port = argv[1];
+    return pingServerSocket(port);
 }
--- pingSocket/ping_client.py
+++ pingSocket/ping_client.py
@@ -3,8 +3,15 @@ import argparse
 import struct
 import time
 
+MESSAGE_LENGTH = 1024
+SLEEP_BETWEEN_MESSAGES = 0.1
 
 def client_connection(ip, port):
+    """
+    Function that opens a client, sends PING to the server and prints its answer
+    ip: string of the server's ip
+    port: int of the server's port
+    """
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((ip, port))
@@ -12,9 +19,9 @@ def client_connection(ip, port):
             message = "PING".encode()
             n = len(message)
             s.send(struct.pack('=I', n))
-            time.sleep(0.1)
+            time.sleep(SLEEP_BETWEEN_MESSAGES)
             s.send(message)
-            data = s.recv(1024)
+            data = s.recv(MESSAGE_LENGTH)
             print(data.decode())
 
     except Exception as e:
@@ -23,6 +30,9 @@ def client_connection(ip, port):
 
 
 def get_arguments():
+    """
+    Function to parse command line arguments (ip and port)
+    """
     parser = argparse.ArgumentParser(description="Client of domain")
     parser.add_argument("ip", type=str, help="ip of server to connect to")
     parser.add_argument("port", type=int, help="port of server to connect to")
